0fce0209bcc3709b155c72c57bd0fd35f06047a4
ASWIN SAMUVEL E <aswin42samuel7@gmail.com>
2025-03-03 22:05:10 +0530

    extension working

--- Component/src/main.jsx
+++ Component/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 //import {Name ,Hello} from './App.jsx'  //importing the different functions from same component
-import {Name as Identity ,Hello as Greet} from './App.jsx'
+import {Name as Identity ,Hello as Greet} from './App.jsx'//extension is not necessary
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
