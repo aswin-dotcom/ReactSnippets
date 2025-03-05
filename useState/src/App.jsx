5d02fa75009d2ffc4b2474ddad5e95400d63704e
ASWIN SAMUVEL E <aswin42samuel7@gmail.com>
2025-03-05 23:01:31 +0530

    Use State

---
+++ useState/src/App.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react'
+import { useState } from 'react'
+
+function App() {
+  const[num,setNum]=useState(1);
+  const handleclick=()=>{
+    setNum((currVal)=>{
+      return currVal+1;
+    })
+  }
+  const  handlecremove = () =>{
+    setNum((currVal)=>{
+      return currVal-1;
+    })
+  }
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onClick={handleclick}>Add</button>
+      <button onClick={handlecremove}>substract</button>
+    </div>
+  )
+}
+
+export default App
