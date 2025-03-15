cbdf54ebdb14eced474c9b522c398d1d1a361d75
ASWIN SAMUVEL E <aswin42samuel7@gmail.com>
2025-03-15 09:43:15 +0530

    useNavigate Updated

--- Router/src/Componets/Navbar.jsx
+++ Router/src/Componets/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+  const navigate =  useNavigate();
+  function handleroute(){
+     //api calll
+     navigate("/users")
+
+  }
   return (
     <div>
         <Link to="/">Home</Link>
-        <Link to="/users">Users</Link>
+        {/* <Link to="/users">Users</Link> */}
+        <li onClick={handleroute}>Users</li>
     </div>
   )
 }
